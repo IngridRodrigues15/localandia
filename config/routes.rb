6bcbb2e2f221fc3de7991da1a6aa8c2d324b6dd4
Ingrid <ingrid.s.rodrigues@hotmail.com>
2020-08-17 15:28:03 -0300

    Action cable setup up

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  # Serve websocket cable requests in-process
+  mount ActionCable.server => '/cable'
+
   root to: "home#index"
   resources :sheets, only: [:new, :create, :show] do
     member do
