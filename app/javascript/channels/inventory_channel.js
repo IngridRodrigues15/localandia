7b3121ba2f83083a1dd98980b0c2c03028828dc8
Ingrid Rodrigues <ingrid.s.rodrigues@hotmail.com>
2020-09-01 20:14:52 -0300

    update player rubies on inventory changes

--- app/javascript/channels/inventory_channel.js
+++ app/javascript/channels/inventory_channel.js
@@ -30,15 +30,19 @@ consumer.subscriptions.create("InventoryChannel", {
     // Atualiza os rubies na tela do mestre
     $("input[data-sheet-id="+sheet+"]").attr('value', rubies);
     $("input[data-sheet-id="+sheet+"]").val(rubies);
+
     //Adiciona o item na ficha na tela do mestre
     if ( $("tbody[data-sheet-id="+sheet+"] tr[data-id="+item_id+"]").length ) {
       $("tbody[data-sheet-id="+sheet+"] tr[data-id="+item_id+"] td:last-child").html(item_quantity)
-
     } else {
       $("tbody[data-sheet-id="+sheet+"]").append('<tr data-id='+item_id+'><td>'+item_name+'</td><td>'+item_description+'</td><td>'+item_quantity+'</td></tr>');
-
     }
 
+    console.log(sheet);
+    console.log(rubies);
+
+    // Atualiza os rubies na tela do jogador
+    $("#sheet_rubies_value[data_sheet_id="+sheet+"]").text(rubies);
 
   }
 });
